6c959b28cff3d95027640f861b30c362ea4a67eb
Jacob Pinchook <jacob.pinchook@gmail.com>
2025-10-06 18:11:04 -0400

    Added form reset on button click.

--- src/index.js
+++ src/index.js
@@ -4,3 +4,7 @@ const createAccountButton = document.querySelector(".create-account");
 createAccountButton.addEventListener('click', (event) => {
    console.log(formOutput.value);
 });
+
+function resetForm() {
+    document.getElementById("signUpForm").reset();
+}
